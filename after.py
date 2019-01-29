8bf58eb5843a4af167090d783a26ba0c3e9162d9
Kavya <32064926+kavya98527@users.noreply.github.com>
2019-01-29 13:05:38 +0530

    Create after.py

---
+++ after.py
@@ -0,0 +1,11 @@
+print("This line will be printed.")
+#num  = float(input("enter a number: "))
+#
+#if num >0:
+#  print ("number is positive")
+#elif num == 0:
+# print ("number is zero")
+#else:
+# print ("number is negative")
+print("new line added for checking") 
+# added new
